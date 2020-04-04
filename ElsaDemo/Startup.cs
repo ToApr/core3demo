c93f8263d00bbffcc98dfa8603fbe53f93496fe4
liufei <229439551@qq.com>
2020-04-04 18:40:11 +0800

    elsa-workflows demo

---
+++ ElsaDemo/Startup.cs
@@ -0,0 +1,67 @@
+using Elsa.Activities.Email.Extensions;
+using Elsa.Activities.Http.Extensions;
+using Elsa.Activities.Timers.Extensions;
+using Elsa.Dashboard.Extensions;
+using Elsa.Persistence.EntityFrameworkCore.DbContexts;
+using Elsa.Persistence.EntityFrameworkCore.Extensions;
+using ElsaDemo.activites;
+using ElsaDemo.Models;
+using ElsaDemo.workflow;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore.SqlServer;
+namespace ElsaDemo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            services.AddActivity<TOAActivites>();
+            services
+          // Add services used for the workflows runtime.
+          .AddElsa(elsa => elsa.AddEntityFrameworkStores<SqlServerContext>(options => options.UseSqlServer(Configuration.GetConnectionString("SqlServer"))))
+//.AddElsa(elsa => elsa.AddEntityFrameworkStores<SqliteContext>(options => options.UseSqlite(Configuration.GetConnectionString("Sqlite"))))
+
+.AddHttpActivities(options => options.Bind(Configuration.GetSection("Elsa:Http")))
+                .AddEmailActivities(options => options.Bind(Configuration.GetSection("Elsa:Smtp")))
+                .AddTimerActivities(options => options.Bind(Configuration.GetSection("Elsa:Timers")))
+                .AddWorkflow<DocumentApprovalWorkflow>()
+                //.AddElsa()
+                ;
+                
+                
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            //// Register necessary ASP.NET Core middleware that triggers workflows containing HTTP activities. 
+           
+            app.UseStaticFiles();
+            app.UseHttpActivities();
+            app.UseRouting();
+            app.UseEndpoints(endpoints => endpoints.MapControllers());
+            // app.UseAuthorization();
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapControllerRoute(
+            //        name: "default",
+            //        pattern: "{controller=Home}/{action=Index}/{id?}");
+            //});
+        }
+    }
+}
---
+++ ElsaDemo/activites/TOAActivites.cs
@@ -0,0 +1,37 @@
+﻿using Elsa;
+using Elsa.Attributes;
+using Elsa.Results;
+using Elsa.Services;
+using Elsa.Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ElsaDemo.activites
+{
+    [ActivityDefinition(
+        Category = "WORKFLOWS",
+        DisplayName = "TOA",
+        Description = "TOA审批环节",
+        Icon = "fas fa-cog",
+        Outcomes = new[] { "Success", "NextTOA" }
+    )]
+    public class TOAActivites: Activity
+    {
+        [ActivityProperty(Name =  "TOA",Type ="")]
+        public string TOA => "TOA";
+        public string Message
+        {
+            get => GetState<string>();
+            set => SetState(value);
+        }
+        protected override ActivityExecutionResult OnExecute(WorkflowExecutionContext context)
+        {
+
+            var r= false;
+            string s= r== true ? "Success" : "NextTOA"; 
+            return Outcome(s); 
+        }
+    }
+}
